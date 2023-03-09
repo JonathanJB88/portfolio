8a1250f5cba63c60392d9ad9bccecc180e63b219
Jonathan Bracho <jonajes0288@gmail.com>
2023-03-09 12:35:12 +0100

    deploy

--- pages/api/services.ts
+++ pages/api/services.ts
@@ -1,8 +1,8 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { services } from "../../data";
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { services, aboutSection, languages, projects, tools } from '../../data';
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ services });
+  res.status(200).json({ services, aboutSection, languages, projects, tools });
 };
 
 export default handler;
